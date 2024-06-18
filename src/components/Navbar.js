388db29fc060577585f881ae2465aff5be51a67a
Mirza Abdullah Baig <84795041+AlwaysBlue-dev@users.noreply.github.com>
2024-06-18 17:36:55 +0500

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -32,44 +32,44 @@ const Navbar = () => {
                 <div className="collapse navbar-collapse" id="navbarBottom">
                     <ul className="navbar-nav m-auto">
                         <li className="nav-item active">
-                            <a className="nav-link" href="#" target="_blank">Home</a>
+                            <a className="nav-link" href="#" >Home</a>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="careersDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" target="_blank">
+                            <a className="nav-link dropdown-toggle" href="#" id="careersDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
                                 HR & Benefits
                             </a>
                             <div className="dropdown-menu" aria-labelledby="careersDropdown">
-                                <a className="dropdown-item" href="#" target="_blank">Payroll</a>
-                                <a className="dropdown-item" href="#" target="_blank">View Payslips</a>
-                                <a className="dropdown-item" href="#" target="_blank">Tax Documents</a>
-                                <a className="dropdown-item" href="#" target="_blank">Benefits</a>
-                                <a className="dropdown-item" href="#" target="_blank">Health Insurance</a>
-                                <a className="dropdown-item" href="#" target="_blank">Leave Management</a>
-                                <a className="dropdown-item" href="#" target="_blank">Request Leave</a>
-                                <a className="dropdown-item" href="#" target="_blank">Performance Reviews</a>
-                                <a className="dropdown-item" href="#" target="_blank">Goals</a>
+                                <a className="dropdown-item" href="#" >Payroll</a>
+                                <a className="dropdown-item" href="#" >View Payslips</a>
+                                <a className="dropdown-item" href="#" >Tax Documents</a>
+                                <a className="dropdown-item" href="#" >Benefits</a>
+                                <a className="dropdown-item" href="#" >Health Insurance</a>
+                                <a className="dropdown-item" href="#" >Leave Management</a>
+                                <a className="dropdown-item" href="#" >Request Leave</a>
+                                <a className="dropdown-item" href="#" >Performance Reviews</a>
+                                <a className="dropdown-item" href="#" >Goals</a>
                             </div>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="promotionsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" target="_blank">
+                            <a className="nav-link dropdown-toggle" href="#" id="promotionsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
                                 Time & Attendance
                             </a>
                             <div className="dropdown-menu" aria-labelledby="promotionsDropdown">
                                 <a className="dropdown-item" href="https://accounts.intuit.com/app/sign-in?app_group=QBTime&asset_alias=Intuit.qbshared.tsheets#" target="_blank">Clock In/Out</a>
-                                <a className="dropdown-item" href="#" target="_blank">Timesheets</a>
-                                <a className="dropdown-item" href="#" target="_blank">Attendance Records</a>
+                                <a className="dropdown-item" href="#" >Timesheets</a>
+                                <a className="dropdown-item" href="#" >Attendance Records</a>
                             </div>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="linksDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" target="_blank">
+                            <a className="nav-link dropdown-toggle" href="#" id="linksDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
                                 Tasks & Projects
                             </a>
                             <div className="dropdown-menu" aria-labelledby="linksDropdown">
-                                <a className="dropdown-item" href="#" target="_blank">To-Do Lists</a>
-                                <a className="dropdown-item" href="#" target="_blank">Project Management</a>
-                                <a className="dropdown-item" href="#" target="_blank">Current Projects</a>
-                                <a className="dropdown-item" href="#" target="_blank">Calendar</a>
-                                <a className="dropdown-item" href="#" target="_blank">Current Projects</a>
+                                <a className="dropdown-item" href="#" >To-Do Lists</a>
+                                <a className="dropdown-item" href="#" >Project Management</a>
+                                <a className="dropdown-item" href="#" >Current Projects</a>
+                                <a className="dropdown-item" href="#" >Calendar</a>
+                                <a className="dropdown-item" href="#" >Current Projects</a>
                             </div>
                         </li>
                         <li className="nav-item dropdown">
@@ -77,28 +77,28 @@ const Navbar = () => {
                                 Training & Development
                             </a>
                             <div className="dropdown-menu" aria-labelledby="promotionsDropdown">
-                                <a className="dropdown-item" href="#" target="_blank">Learning Management System (LMS)</a>
-                                <a className="dropdown-item" href="#" target="_blank">Courses</a>
-                                <a className="dropdown-item" href="#" target="_blank">Certifications</a>
-                                <a className="dropdown-item" href="#" target="_blank">Training Calendar</a>
-                                <a className="dropdown-item" href="#" target="_blank">Resource Library</a>
+                                <a className="dropdown-item" href="#">Learning Management System (LMS)</a>
+                                <a className="dropdown-item" href="#">Courses</a>
+                                <a className="dropdown-item" href="#">Certifications</a>
+                                <a className="dropdown-item" href="#">Training Calendar</a>
+                                <a className="dropdown-item" href="#">Resource Library</a>
                             </div>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="meetingsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" target="_blank">
+                            <a className="nav-link dropdown-toggle" href="#" id="meetingsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Documents
                             </a>
                             <div className="dropdown-menu" aria-labelledby="meetingsDropdown">
-                                <a className="dropdown-item" href="#" target="_blank">Company Policies</a>
-                                <a className="dropdown-item" href="#" target="_blank">Employee Handbook</a>
-                                <a className="dropdown-item" href="#" target="_blank">Compliance Documents</a>
-                                <a className="dropdown-item" href="#" target="_blank">Personal Documents</a>
-                                <a className="dropdown-item" href="#" target="_blank">Upload Documents</a>
-                                <a className="dropdown-item" href="#" target="_blank">View Uploaded Documents</a>
+                                <a className="dropdown-item" href="#">Company Policies</a>
+                                <a className="dropdown-item" href="#">Employee Handbook</a>
+                                <a className="dropdown-item" href="#">Compliance Documents</a>
+                                <a className="dropdown-item" href="#">Personal Documents</a>
+                                <a className="dropdown-item" href="#">Upload Documents</a>
+                                <a className="dropdown-item" href="#">View Uploaded Documents</a>
                             </div>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="meetingsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" target="_blank">
+                            <a className="nav-link dropdown-toggle" href="#" id="meetingsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Quick Links
                             </a>
                             <div className="dropdown-menu" aria-labelledby="meetingsDropdown">
@@ -107,18 +107,18 @@ const Navbar = () => {
                                 <a className="dropdown-item" href="https://accounts.intuit.com/app/sign-in?app_group=QBTime&asset_alias=Intuit.qbshared.tsheets#" target="_blank">Quickbooks Time (Clock In)</a>
                                 <a className="dropdown-item" href="https://accounts.intuit.com/app/sign-in?app_group=Workforce&asset_alias=Intuit.smallbusiness.vmp.employeewidgets&redirect_uri=https%3A%2F%2Fpaychecks.intuit.com%2Fapp%2Fpayroll-employee-portal-ui%2Fportal%2Fpaychecks&single_sign_on=false&appfabric=true" target="_blank">Workforce</a>
                                 <a className="dropdown-item" href="https://accounts.intuit.com/app/sign-in?app_group=Workforce&asset_alias=Intuit.smallbusiness.vmp.employeewidgets&redirect_uri=https%3A%2F%2Fpaychecks.intuit.com%2Fapp%2Fpayroll-employee-portal-ui%2Fportal%2Fpaychecks&single_sign_on=false&appfabric=true" target="_blank">Payroll</a>
-                                <a className="dropdown-item" href="" target="_blank">Mail</a>
+                                <a className="dropdown-item" href="" >Mail</a>
                             </div>
                         </li>
                         <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="#" id="promotionsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" target="_blank">
+                            <a className="nav-link dropdown-toggle" href="#" id="promotionsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
                                 Profile
                             </a>
                             <div className="dropdown-menu" aria-labelledby="promotionsDropdown">
-                                <a className="dropdown-item" href="#" target="_blank">Personal Information</a>
-                                <a className="dropdown-item" href="#" target="_blank">Contact Details</a>
-                                <a className="dropdown-item" href="#" target="_blank">Emergency Contacts</a>
-                                <a className="dropdown-item" href="#" target="_blank">Banking Information</a>
+                                <a className="dropdown-item" href="#" >Personal Information</a>
+                                <a className="dropdown-item" href="#" >Contact Details</a>
+                                <a className="dropdown-item" href="#" >Emergency Contacts</a>
+                                <a className="dropdown-item" href="#" >Banking Information</a>
                             </div>
                         </li>
                     </ul>
